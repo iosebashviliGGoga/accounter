515530e63bd6a66b15d6a10ad813d0dd90c26fd8
iosebashviliggoga <iosebashviliggoga@gmail.com>
2024-05-12 19:07:08 +0400

    added profile styles

--- app/user/page.js
+++ app/user/page.js
@@ -1,19 +1,21 @@
 export default function Page() {
     return <>
 
-        <div className="userCard">
-            <div className="w-100 h-100 d-flex justify-content-center align-items-center">
-                <div>
-                    <h2 className="userCard--header">შეიყვანეთ მონაცემები:</h2>
+        <div className="userCard--wrapper">
+            <div className="userCard">
+                <div className="w-100 h-100 d-flex justify-content-center align-items-center">
+                    <div>
+                        <h2 className="userCard--header">შეიყვანეთ მონაცემები:</h2>
 
-                    <form action={''} className="d-flex flex-column align-items-center">
-                        <div className="input--container">
-                            <input type="text" name="email" className="form-control" placeholder="ელ-ფოსტა" />
-                            <input type="text" name="password" className="form-control " placeholder="პაროლი" />
-                            <input type="text" name="mobile" className="form-control " placeholder="ტელეფონის ნომერი" />
-                        </div>
-                        <button className="usercard--submitBtn">გაგრძელება</button>
-                    </form>
+                        <form action={''} className="d-flex flex-column align-items-center">
+                            <div className="input--container">
+                                <input type="text" name="email" className="form-control" placeholder="ელ-ფოსტა" />
+                                <input type="text" name="password" className="form-control " placeholder="პაროლი" />
+                                <input type="text" name="mobile" className="form-control " placeholder="ტელეფონის ნომერი" />
+                            </div>
+                            <button className="usercard--submitBtn">გაგრძელება</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
--- app/user/profile/page.js
+++ app/user/profile/page.js
@@ -1,8 +1,181 @@
 "use client"
+import { useState } from 'react';
+import IconButton from '@mui/material/IconButton';
 
-function Page(){
+import FilledInput from '@mui/material/FilledInput';
+import InputLabel from '@mui/material/InputLabel';
+import Switch from '@mui/material/Switch';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import { styled } from '@mui/material/styles';
+import { PaymentInputsWrapper, usePaymentInputs } from 'react-payment-inputs';
+import images from 'react-payment-inputs/images';
+function Page() {
+    const [showPassword, setShowPassword] = useState(false);
 
-    return <div className="userCard">profile</div>
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+    const {
+        wrapperProps,
+        getCardImageProps,
+        getCardNumberProps,
+        getExpiryDateProps,
+        getCVCProps
+    } = usePaymentInputs();
+
+
+    const IOSSwitch = styled((props) => (
+        <Switch focusVisibleClassName=".Mui-focusVisible"  {...props} />
+    ))(({ theme }) => ({
+        width: 136,
+        height: 42,
+        padding: 0,
+        '& .MuiSwitch-switchBase': {
+            padding: 0,
+            margin: 2,
+            color: '#9C9C9C',
+            transitionDuration: '300ms',
+            '&.Mui-checked': {
+                transform: 'translateX(72px)',
+                color: '#9C9C9C',
+                '& + .MuiSwitch-track': {
+                    backgroundColor: theme.palette.mode === 'dark' ? '#2ECA45' : '#65C466',
+                    opacity: 1,
+                    border: 0,
+                    borderRadius: '21px', // Add border-radius to the track here
+                },
+                '&.Mui-disabled + .MuiSwitch-track': {
+                    opacity: 0.5,
+                },
+            },
+            '&.Mui-focusVisible .MuiSwitch-thumb': {
+                color: '#33cf4d',
+                border: '6px solid #fff',
+            },
+            '&.Mui-disabled .MuiSwitch-thumb': {
+                color: theme.palette.mode === 'light' ? theme.palette.grey[100] : theme.palette.grey[600],
+            },
+            '&.Mui-disabled + .MuiSwitch-track': {
+                opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
+            },
+        },
+        '& .MuiSwitch-thumb': {
+            boxSizing: 'border-box',
+            width: 59,
+            height: 38,
+            borderRadius: '21px', // Add border-radius to the thumb here
+        },
+        '& .MuiSwitch-track': {
+            backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
+            opacity: 1,
+            borderRadius: '21px', // Ensure border-radius is set for track here as well
+            transition: theme.transitions.create(['background-color'], {
+                duration: 500,
+            }),
+        },
+    }));
+
+    return <div className="userCard--wrapper">
+
+        <div className="userCard">
+
+            <div className="profile--header">
+                <div className="profile--initals">
+                    <span>გბ</span>
+                </div>
+                <h2>გიორგი ბერიძე</h2>
+            </div>
+            <div className="profile--inputs">
+                <div className="profile--input">
+                    <label className="form-label">სახელი</label>
+                    <input type="text" className="form-control" value="გიორგი" />
+                </div>
+                <div className="profile--input">
+                    <label className="form-label">ტელეფონის ნომერი</label>
+                    <input type="text" className="form-control" value="557-567-999" />
+                </div>
+                <div className="profile--input">
+                    <label className="form-label">გვარი</label>
+                    <input type="text" className="form-control" value="ბერიძე" />
+                </div>
+                <div className="profile--input">
+                    <label className="form-label">RS მომხმარებელი</label>
+                    <input type="text" className="form-control" value="RS-0102556986" />
+                </div>
+                <div className="profile--input">
+                    <label className="form-label">ელ-ფოსტა</label>
+                    <input type="text" className="form-control" value="giorgi.beridze@gmailcom" />
+                </div>
+                <div className="profile--input">
+                    <label className="form-label">პაროლი</label>
+                    <FormControl variant="filled">
+
+                        <FilledInput
+
+                            id="filled-adornment-password"
+                            value={`giorgi`}
+                            type={showPassword ? 'text' : 'password'}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                        edge="end"
+                                    >
+                                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                        />
+                    </FormControl>
+                </div>
+                <div className="profile--input">
+                    <label className="form-label">შეტყობინებების მიღება</label>
+                    <div className="d-block">
+                        <FormControlLabel
+                            control={<IOSSwitch sx={{ m: 1 }} defaultChecked />}
+
+                        />
+                    </div>
+                </div>
+                <div className="profile--input">
+                    <label className="form-label">ბარათი</label>
+
+                    <PaymentInputsWrapper {...wrapperProps} id="cardinput--wrapper">
+                        <svg {...getCardImageProps({ images })} />
+                        <input
+
+                            value={`5483 8803 5431 2523`}
+                            
+                        />
+                    </PaymentInputsWrapper>
+                    <div className="cardinput--delete">
+                        <span>
+                            ბარათის წაშლა
+                        </span>
+                        <img src="/assets/images/delete.svg" alt="" />
+                    </div>
+                </div>
+
+            </div>
+
+            <div className="profile--submitWrapper">
+                <button className='usercard--submitBtn'>რედაქტირება</button>
+            </div>
+
+
+
+
+
+        </div>
+    </div>
 }
 
 export default Page
